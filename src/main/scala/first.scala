a46a27d46d5ff718e9c262fb4b443f4cd6322975
sivaramakrishnakakarla <sivakakarla777@gmail.com>
2025-01-14 10:17:47 -0500

    my first program

--- src/main/scala/first.scala
+++ src/main/scala/first.scala
@@ -1,6 +1,12 @@
 object first {
   def main(args: Array[String]): Unit = {
-    print("hello world")
+    val arr = Array(10,20,30,40,50,60,70,80,90, 'a')
+    var array = Array('a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 1)
+    for (i <- 0 until arr.length) {
+      for (j<- 0 until array.length)
+        println(array(j))
+      println(arr(i))
+    }
   }
 
 }
